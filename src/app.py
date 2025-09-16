8cbe36fe19ca371669964693de49cb6cb64f531e
Ben Wilson <benwilson@Lisas-MacBook-Pro.local>
2025-09-16 16:35:02 -0700

    minimal echo app

--- src/app.py
+++ src/app.py
@@ -2,21 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.get("/")
 def index():
     return """
-    <h2>Bye-Week Scout</h2>
+    <h2>Bye-Week Scout </h2>
     <form action="/echo" method="POST">
-        <label>Type something:</label>
-        <input name="user_input">
-        <button type="submit">Submit</button>
+      <label>Type something:</label>
+      <input name="user_input">
+      <button type="submit">Submit</button>
     </form>
     """
 
-@app.route("/echo", methods=["POST"])
+@app.post("/echo")
 def echo():
     text = request.form.get("user_input", "")
     return "You entered: " + text
-
-if __name__ == "__main__":
-    app.run()
