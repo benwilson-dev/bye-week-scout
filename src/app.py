f80d891b225d98cfb5cf7067db6a46fa29b5b4f7
Ben Wilson <benwilson@Lisas-MacBook-Pro.local>
2025-09-16 17:19:26 -0700

    handle GET /echo; minimal echo page

--- src/app.py
+++ src/app.py
@@ -1,19 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
     return """
-    <h2>Bye-Week Scout </h2>
+    <h2>Bye-Week Scout</h2>
     <form action="/echo" method="POST">
       <label>Type something:</label>
-      <input name="user_input">
+      <input name="user_input" />
       <button type="submit">Submit</button>
     </form>
     """
 
 @app.post("/echo")
-def echo():
+def echo_post():
     text = request.form.get("user_input", "")
     return "You entered: " + text
+
+@app.get("/echo")
+def echo_get():
+    return redirect(url_for("index"))
